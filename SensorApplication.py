917462aa8593ce35d77ab464f56c25a18fa6012d
briannaorr1 <90328730+briannaorr1@users.noreply.github.com>
2023-11-26 05:46:11 -0400

    created unit tests

--- SensorApplication.py
+++ SensorApplication.py
@@ -188,7 +188,6 @@ class Sensor:
             cursor.execute(update_query, (i_NewState, self.s_SerialNumber))
             conn.commit()
             print(f"Sensor '{self.s_SerialNumber}' errorflag changed to {i_NewState}")
-            schedule_technician()
         except mysql.connector.Error as err:
             print(f"MySQL error: {err}")
         except Exception as e:
@@ -563,17 +562,136 @@ def update_schedule_in_database(excel_file, technician_id):
             print(f"Error getting sensor data: {e}")
 
 
-def retrieve_earliest_opentime(): 
-    current_day = datetime.now().weekday()  
-    current_day_col = ['availabilitySunday', 'availabilityMonday', 'availabilityTuesday', 'availabilityWednesday', 'availabilityThursday', 'availabilityFriday', 'availabilitySaturday'][current_day]
-    current_time = datetime.now().strftime('%H:%M:%S')
-    earliest_open_entry_query = f""" SELECT technicianID, MIN(timeInHours) AS earliest_time
-        FROM schedule WHERE {current_day_col} = 'Y' AND timeInHours > '{current_time}'
-        GROUP BY technicianID
-    """
-    cursor.execute(earliest_open_entry_query)
-    earliest_open_times = cursor.fetchall()
-    print(earliest_open_times)
+# Function to retrieve user's schedule from the database
+def getSchedule(i_UserID):
+    try:
+        select_query = "SELECT timeInHours, availabilityMonday, availabilityTuesday, availabilityWednesday, availabilityThursday, availabilityFriday FROM schedule WHERE technicianID = %s"
+
+        cursor.execute(select_query, (i_UserID,))
+        sensor_data = cursor.fetchall()
+
+        if sensor_data:  # if there is an entry
+            rows = []
+            for row in sensor_data:
+                timeInHours, arr_Monday, arr_Tuesday, arr_Wednesday, arr_Thursday, arr_Friday = row
+    
+                new_row = {
+                    "Hours": timeInHours,
+                    "Monday": str(arr_Monday),
+                    "Tuesday": str(arr_Tuesday),
+                    "Wednesday": str(arr_Wednesday),
+                    "Thursday": str(arr_Thursday),
+                    "Friday": str(arr_Friday)
+                }
+
+                rows.append(new_row)
+
+            df = pd.concat([pd.DataFrame(rows)], ignore_index=True)
+            df = df.replace({'Y': 1, 'N': 0})
+            print(df)
+            return df
+        else:
+            print(f"No data found for Technician with ID '{i_UserID}'")
+    except mysql.connector.Error as e:
+        print(f"MySQL Error: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+
+def find_next_day_of_week(current_day):
+    # Define a mapping from day names to corresponding integers
+    days_mapping = {
+        'monday': 0,
+        'tuesday': 1,
+        'wednesday': 2,
+        'thursday': 3,
+        'friday': 4,
+        'saturday': 5,
+        'sunday': 6
+    }
+
+    # Convert the input day to lowercase and get its corresponding integer value
+    current_day = current_day.lower()
+    current_day_index = days_mapping.get(current_day)
+
+    if current_day_index is not None:
+        # Get the current date and day of the week
+        today = datetime.now()
+        current_day_of_week = today.weekday()
+
+        # Calculate the number of days until the next occurrence of the input day
+        days_until_next = (current_day_index - current_day_of_week + 7) % 7
+
+        # Calculate the next date
+        next_date = today + timedelta(days=days_until_next)
+
+        return next_date.year, next_date.strftime('%B'), next_date.day
+    else:
+        return "Invalid day name"
+
+from datetime import datetime, time
+
+def getEarliestAvailability(df):
+    
+    # Set the desired time
+    desired_time = time(hour=14, minute=00, second=0)
+    current_date = datetime.now().date()
+    current_time = datetime.combine(current_date, desired_time)
+    print("Manually set current time:", current_time)
+
+    current_day = current_time.weekday()
+    day_of_week = current_time.strftime('%A')
+    current_hour = current_time.hour
+    next_hour = current_hour + 1 
+    
+    filtered_df = df[df['Hours'] == (f"0 days {next_hour}:00:00")]
+    hour_and_day = filtered_df[f"{day_of_week}"]
+
+    next_day = None
+    next_day_of_week = None
+    next_hour = None
+
+    if hour_and_day.iat[0] == 1:
+        next_day = current_day
+        next_day_of_week = day_of_week
+        next_hour = current_hour + 1 
+        next_hour = (f"0 days {next_hour}:00:00")
+    else: # keep searching
+        tomorrow = current_time + timedelta(days=1)
+        day_of_week_tomorrow = tomorrow.strftime('%A')
+
+        if day_of_week_tomorrow == "Saturday":
+            next_day = current_time + timedelta(days=3)
+        elif day_of_week_tomorrow == "Sunday":
+            next_day = current_time + timedelta(days=2)
+        else:
+            next_day = current_time + timedelta(days=1)
+
+        next_day_of_week = next_day.strftime('%A')
+        
+        for index, row in df.iterrows():
+            entry = row[next_day_of_week]
+            if entry == 1:
+                next_hour = row['Hours']
+                break
+        
+    s_Year, s_Month, s_Day = find_next_day_of_week(next_day_of_week)
+    s_AvailabilityString = f"{next_day_of_week} {s_Month} {s_Day}, {s_Year} at {str(next_hour)[7:]}"
+    print(f"Next Time: {next_day_of_week} {s_Month} {s_Day}, {s_Year} at {str(next_hour)[7:]}")
+    return next_day_of_week, s_Month, s_Day, s_Year, str(next_hour)[7:], s_AvailabilityString
+    
+def Book(i_UserID, s_DayOfWeek, s_Hour, s_AvailabilityString):
+    try:       
+        update_query = f"UPDATE schedule SET availability{s_DayOfWeek} = %s WHERE timeInHours = %s AND technicianID = %s"
+        s_Time = timedelta(days=0, hours=int(s_Hour[0:2]), minutes=0)
+        print(s_Time)
+        cursor.execute(update_query, ('N', s_Time, i_UserID))
+        conn.commit()
+        print(f"Success! Technician with ID '{i_UserID}' is Booked for {s_AvailabilityString}")
+    except mysql.connector.Error as err:
+        print(f"MySQL error: {err}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {str(e)}")
+
 
 
 ############### END: FUNCTIONS ###################
@@ -609,16 +727,11 @@ def main():
     #get_Out_Of_Bounds_Sensors()
     #retrieve_earliest_opentime()
     #schedule_technician()
-    update_schedule_in_database(schedule, 1)
-
-    '''
-    technician_ids = []
-    select_query = "SELECT id FROM user WHERE userType = %s"
-    cursor.execute(select_query, ("Technician",))
-    user_data = cursor.fetchall() 
-    for i in len(user_data): #check if schedule is already in the database 
-        technician_ids.append(user_data[i][0])
-    '''
+    #update_schedule_in_database(schedule, 1)
+    df = getSchedule(i_UserID=2)
+    s_DayOfWeek, s_Month, s_Day, s_Year, s_Hour, s_AvailabilityString = getEarliestAvailability(df)
+    #Book(i_UserID=1, s_DayOfWeek=s_DayOfWeek, s_Hour=s_Hour, s_AvailabilityString=s_AvailabilityString)
+    
     
     
 if __name__ == "__main__":
---
+++ UnitTests.py
@@ -0,0 +1,240 @@
+import unittest
+from SensorApplication import *
+from SensorApplication import Sensor 
+
+# Initialize Database Connections
+conn = mysql.connector.connect(user=s_User, password=s_Password, host=s_Host, database=s_DatabasePath)
+cursor = conn.cursor()
+
+#Constants
+sensor = getSensor("Water_LakeHuron_S0003")
+s_Timestamp = "2023-11-08 14:07:28"
+s_Timestamp2 = "2023-11-15 16:41:23"
+class UnitTests(unittest.TestCase):
+    
+    def print_test_result(self):
+        test_name = self.id().split('.')[-1]  # Get the name of the current test
+        if self._outcome.success:
+            print(f"'{test_name}' passed.")
+        else:
+            print(f"'{test_name}' failed.")
+
+   
+    '''
+    def test_generate_serial_number():
+        print("Test_case")
+      #Possible Test -  confirm that the  number generated is valid
+    '''
+    '''
+    #Function to test get_type
+    def test_get_type(self):
+        result = sensor.get_type()
+        expected_type = sensor.s_SensorType
+        self.assertEqual(result, expected_type)
+        self.print_test_result()
+
+    #Function to test get_value
+    def test_get_value(self):
+        result = sensor.get_value(s_Timestamp)[1]
+        select_query = "SELECT val FROM value WHERE timestamp = %s AND serialnum = %s"
+        cursor.execute(select_query, (s_Timestamp, sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given timestamp and serial number")
+        expected_value = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test set_value
+    def test_set_value(self):
+        expected_value = 1.14
+        sensor.set_value(s_Timestamp, expected_value)
+        select_query = "SELECT val FROM value WHERE timestamp = %s AND serialnum = %s"
+        cursor.execute(select_query, (s_Timestamp, sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given timestamp and serial number")
+        result = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    
+    #Function to test get_last_sampled_time, TEST FAILS
+    def test_get_last_sampled_time(self):
+        result = sensor.get_last_sampled_time()
+        expected_value = "2023-11-09 20:09:31"
+        self.assertEqual(result, expected_value)
+    
+    #Function to test get_errorflag, TEST FAILS
+    def test_get_errorflag(self):
+        result = sensor.get_errorflag()
+        select_query = "SELECT errorflag FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given sensor")
+        expected_value = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test set_errorflag
+    def test_set_errorflag(self):
+        expected_value = 0
+        sensor.set_errorflag(expected_value)
+        select_query = "SELECT errorflag FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given sensor")
+        result = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test get_status
+    def test_get_status(self):
+        result = sensor.get_status()
+        select_query = "SELECT status FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given sensor")
+        expected_value = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test set_status
+    def test_set_status(self):
+        expected_value = "OFF"
+        sensor.set_status(expected_value)
+        select_query = "SELECT status FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given sensor")
+        result = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test get_sampling_rate
+    def test_get_sampling_rate(self):
+        result = sensor.get_sampling_rate()
+        select_query = "SELECT samplingrate FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (sensor.s_SerialNumber, ))
+        sensor_data = cursor.fetchone()
+        self.assertIsNotNone(sensor_data, "Data was not found for the given sensor")
+        expected_value = sensor_data[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test set_sampling_rate
+    def test_set_sampling_rate(self):
+        expected_value = "5"
+        sensor.set_sampling_rate(expected_value)
+        result = sensor.i_SamplingRate
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+    
+                
+    #Function to test createSensor
+    def test_createSensor(self): 
+        s_SensorType = 'Water'
+        s_Location = 'Bathurst'
+        i_SamplingRate = 1
+        sensor = createSensor(s_SensorType, s_Location, i_SamplingRate)
+
+        self.assertIsInstance(sensor, Sensor)  
+        self.assertEqual(sensor.s_SensorType, s_SensorType)  
+        self.assertEqual(sensor.s_Location, s_Location) 
+        self.assertEqual(sensor.i_SamplingRate, i_SamplingRate)  
+    
+
+    #Function to test getSensor
+    def test_getSensor(self): 
+        serial_num = "Water_LakeHuron_S0003"
+        sensor = getSensor(serial_num)
+        self.assertIsInstance(sensor, Sensor)  
+        self.print_test_result()
+    '''
+
+    #Function to test deleteSensor 
+    def test_delete_sensor(self):
+        serial_num = "Water_LakeHuron_S0035"
+        result = deleteSensor(serial_num)
+        select_query = "SELECT * FROM sensor WHERE serialnumber = %s"
+        cursor.execute(select_query, (serial_num, ))
+        sensor_data = cursor.fetchone()
+        if sensor_data != None: 
+            self.assertTrue(result)  
+        else: 
+            self.assertFalse(result)
+
+    #Function to test getCurrentHistoricalData 
+
+    '''
+    #Function to test total_energy_consumption 
+    def test_total_energy_consumption(self):
+        result = total_energy_consumption()
+        type = "Energy%"
+        sum_query = "SELECT SUM(val) FROM value WHERE serialnum LIKE  %s"
+        cursor.execute(sum_query,(type,))
+        data = cursor.fetchone()
+        self.assertIsNotNone(data, "Data was not found for the given sensor")
+        expected_value= data[0]
+        self.assertEqual(round(result), round(expected_value))
+        self.print_test_result()
+    
+    
+    #Function to test total_energy_consumption at a specific timestamp
+    def test_total_energy_consumption(self):
+        result = total_energy_consumption(s_Timestamp2)
+        type = "Energy%"
+        sum_query = "SELECT SUM(val) FROM value WHERE timestamp = %s AND serialnum LIKE  %s"
+        cursor.execute(sum_query,(s_Timestamp2,type,))
+        data = cursor.fetchone()
+        self.assertIsNotNone(data, "Data was not found for the given sensor")
+        expected_value= data[0]
+        self.assertEqual(round(result), round(expected_value))
+        self.print_test_result()
+    
+    #Function to test total_water_consumption 
+    def test_total_water_consumption(self):
+        result = total_water_consumption()
+        type = "Water%"
+        sum_query = "SELECT SUM(val) FROM value WHERE serialnum LIKE %s"
+        cursor.execute(sum_query,(type,))
+        expected_value= cursor.fetchone()[0]
+        self.assertEqual(round(result), round(expected_value))
+        self.print_test_result()
+
+    #Function to test total_water_consumption at a specific timestamp
+    def test_total_water_consumption(self):
+        result = total_water_consumption(s_Timestamp)
+        type = "Water%"
+        sum_query = "SELECT SUM(val) FROM value WHERE timestamp = %s AND serialnum LIKE  %s"
+        cursor.execute(sum_query,(s_Timestamp,type,))
+        expected_value= cursor.fetchone()[0]
+        self.assertEqual(round(result), round(expected_value))
+        self.print_test_result()
+    
+    #Function to test total_out_of_bounds
+    def test_total_offline(self):
+        result = total_offline()
+        status = "OFF"
+        sum_query = "SELECT COUNT(status) FROM sensor WHERE status =  %s"
+        cursor.execute(sum_query,(status,))
+        expected_value= cursor.fetchone()[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+
+    #Function to test total_out_of_bounds
+    def test_total_out_of_bounds(self):
+        result = total_out_of_bounds()
+        errorflag = 1
+        sum_query = "SELECT COUNT(errorflag) FROM sensor WHERE errorflag =  %s"
+        cursor.execute(sum_query,(errorflag,))
+        expected_value= cursor.fetchone()[0]
+        self.assertEqual(result, expected_value)
+        self.print_test_result()
+        
+    
+    #Function to test get_Out_Of_Bounds_Sensors
+    def get_Out_Of_Bounds_Sensors():
+        result = get_Out_Of_Bounds_Sensors()
+    '''
+    
+if __name__ == '__main__':
+    unittest.main()
