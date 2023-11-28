2b16b94e468a70976f1fe28c0d206818a22c4ed9
Asimiea Sobomate-Victor <85371257+asimiea@users.noreply.github.com>
2023-11-28 16:58:21 -0400

    Update filepath

--- Sensor.py
+++ Sensor.py
@@ -24,7 +24,7 @@ from firebase_admin import db
 import unittest
 ############### END: BASIC IMPORTS ##################
 
-s_ConfigFilePath = 'main/config.json'
+s_ConfigFilePath = 'config.json'
 
 ################### CONFIGURATION ###################
 with open(s_ConfigFilePath, 'r') as config_file:
