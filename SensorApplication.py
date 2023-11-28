d71892ee483161591962c0db6a5d4c6dc630bac3
Asimiea Sobomate-Victor <85371257+asimiea@users.noreply.github.com>
2023-11-28 17:03:28 -0400

    Update filepath

--- SensorApplication.py
+++ SensorApplication.py
@@ -22,7 +22,7 @@ import mysql.connector
 import unittest
 ############### END: BASIC IMPORTS ##################
 
-s_ConfigFilePath = 'tree/main/config.json'
+s_ConfigFilePath = "config.json"
 
 ################### CONFIGURATION ###################
 with open(s_ConfigFilePath, 'r') as config_file:
