b2fc055474f988b794403a93690df6e9e17c1156
ethanriddell <115733213+ethanriddell@users.noreply.github.com>
2023-11-30 13:03:15 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Python app',
+   version='1.0',
+   description='Monitoring sensor status and data',
+   author='SWE4103-Group2',
+   packages=['Python app'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
