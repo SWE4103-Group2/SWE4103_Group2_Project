dbf7301f219521cfc5c27d3901bb4670cfe840ea
ethanriddell <115733213+ethanriddell@users.noreply.github.com>
2023-11-30 13:33:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,11 @@ with open("README.md", 'r') as f:
     description = f.read()
    
 setuptools.setup(
-   name='https://github.com/SWE4103-Group2/SWE4103_Group2_Project',
+   name='SWE4103_Group2_Project',
    version='1.0',
    description=description,
    author='SWE4103-Group2',
-   packages=['https://github.com/SWE4103-Group2/SWE4103_Group2_Project'],  #same as name
+   packages=['SWE4103_Group2_Project'],  #same as name
    install_requires=[], #external packages as dependencies
-    package_dir={sensor-management-app},
-    packages=find_packages("src"),
-    python_requires=">=3.6",
+    url="https://github.com/SWE4103-Group2/SWE4103_Group2_Project",
 )
